4d02bb8582ab82796796b81b8bf14a409b570824
SUDAR2005 <sudarsettaiyan@gmail.com>
2026-02-02 15:33:02 +0000

    new: module 3 output text file; fix: formating and spacing issues in backup_manager.sh

--- linux-assessment/module3/backup_manager.sh
+++ linux-assessment/module3/backup_manager.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 # declare variable for src, dst and ext
-src=$1
-dst=$2
-ext=$3
-
+src=$1; dst=$2; ext=$3
 # check dir exist
 # 1. used command line arguments
 if [ ! -d "$src" ]; then
     echo "src Folder $src is not given or doesn't exist"
     exit 1
 fi
-
 if [ ! -d "$dst" ]; then
     echo "dst Folder $dst doesn't exist, creating one"
     # 5. backup dir doesn't exist create if fails exit with error
@@ -22,7 +18,6 @@ fi
 # 2. used globbing
 # 4. store the names in array
 mapfile -t files < <(find "$src" -type f -name "*$ext")
-
 if [ "${#files[@]}" -eq 0 ]; then
     echo "no .$ext files found in source directory"
     exit 1
@@ -30,23 +25,22 @@ fi
 
 echo "Files found:"
 printf '%s\n' "${files[@]}"
-
 echo "*************************--*****************************"
+
 # 4. print file size and stats related to it
 echo "Stats of files to be backed-up"
 for file in "${files[@]}"; do
     stat -c "%n %s" "$file"
 done
-
 echo "*************************--*****************************"
 
 # backup statistics
 export BACKUP_COUNT=0
 total_size=0
 
-# 5. Backup files
+# 5. backup files
 for file in "${files[@]}"; do
-    # Get just the filename without path for destination
+    # get just the filename without path for destination
     filename=$(basename "$file")
     dest_file="$dst/$filename"
     
@@ -68,9 +62,7 @@ for file in "${files[@]}"; do
     fi
 done
 
-
 report="$dst/backup_report.log"
-
 {
     echo "Backup Summary Report"
     echo "total files processed: ${#files[@]}"
@@ -81,11 +73,6 @@ report="$dst/backup_report.log"
 } > "$report"
 
 cat "$report"
-
-
 echo "*************************--*****************************"
-
 echo "Completed!!! report saved at $report"
-
-
 echo "*************************--*****************************"
